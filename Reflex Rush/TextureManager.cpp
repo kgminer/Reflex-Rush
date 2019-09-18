482fe9e8463793eadae8bccf5133cbcc3dae88b6
kgminer <k.gerhart@comcast.net>
2019-09-17 22:58:52 -0400

    Getting rid of broken version

--- Reflex Rush/TextureManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "TextureManager.h"
-
-SDL_Texture * TextureManager::LoadTexture(const char * fileName, SDL_Renderer * ren)
-{
-	SDL_Surface *tempSuface = IMG_Load(fileName);
-	SDL_Texture*  texture = SDL_CreateTextureFromSurface(ren, tempSuface);
-	SDL_FreeSurface(tempSuface);
-	return texture;
-}
--- Reflex Rush/TextureManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameManager.h"
-
-class TextureManager
-{
-public:
-	static SDL_Texture* LoadTexture(const char* fileName, SDL_Renderer* ren);
-};
-
